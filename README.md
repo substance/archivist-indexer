716e3d759ce30e9613d0364ed3d5bf67ff81dfce
Daniel Beilinson <beilinson@gmail.com>
2015-06-18 01:13:02 +0300

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Archvist Indexer
+
+The Archvist-Indexer represents a lookup service for Archvist's interviews, built on top of an ElasticSearch index, and provides full-text search on interviw meta data (interviewee, abstract, bio etc.) as well as the content of interview fragments.
---
+++ src/index_configuration.js
@@ -0,0 +1,84 @@
+module.exports = {
+  'index': 'interviews',
+  'body': {
+    "settings": {
+      "analysis": {
+        "filter": {
+          "trigrams_filter": {
+            "type": "ngram",
+            "min_gram": 3,
+            "max_gram": 3
+          },
+          "stopwords_ru": {
+            "type": "stop",
+            "stopwords": ["а","без","более","бы","был","была","были","было","быть","в","вам","вас","весь","во","вот", "все", "всего", "всех", "вы", "где", "да", "даже", "для", "до", "его", "ее", "если", "есть", "еще", "же", "за", "здесь", "и", "из", "или", "им", "их", "к", "как", "ко", "когда", "кто", "ли", "либо", "мне", "может", "мы", "на", "надо", "наш", "не", "него", "нее", "нет", "ни", "них", "но", "ну", "о", "об", "однако", "он", "она", "они", "оно", "от", "очень", "по", "под", "при", "с", "со", "так", "также", "такой", "там", "те", "тем", "то", "того", "тоже", "той", "только", "том", "ты", "у", "уже", "хотя", "чего", "чей", "чем", "что", "чтобы", "чье", "чья", "эта", "эти", "это", "я"],
+            "ignore_case": "true"
+          },
+          "custom_word_delimiter": {
+            "type": "word_delimiter",
+            "generate_word_parts": "true",
+            "generate_number_parts": "true",
+            "catenate_words": "true",
+            "catenate_numbers": "false",
+            "catenate_all": "true",
+            "split_on_case_change": "true",
+            "preserve_original": "true",
+            "split_on_numerics": "false"
+          }
+        },
+        "char_filter": {
+          "ru": {
+            "type": "mapping",
+            "mappings": ['Ё=>Е', 'ё=>е']
+          }
+        },
+        "analyzer": {
+          "analyzer_ru": {
+            "type": "custom",
+            "tokenizer": "nGram",
+            "filter": ["stopwords_ru", "stop", "custom_word_delimiter", "lowercase", "russian_morphology", "english_morphology"],
+            "char_filter": ["ru"]
+          },
+          "searcher_ru": {
+            "type": "custom",
+            "tokenizer": "nGram",
+            "filter": ["stopwords_ru", "stop", "custom_word_delimiter", "lowercase", "russian_morphology", "english_morphology"],
+            "char_filter": ["ru"]
+          }
+        },
+        "tokenizer": {
+          "nGram": {
+            "type": "nGram",
+            "min_gram": 4,
+            "max_gram": 20
+          }
+        }
+      }
+    },
+    "mappings": {
+      "interview": {
+       "properties": {
+         // interview abstract and interviewee biography are indexed for fuzzy full-text search
+         "abstract": { "type": "string", "index" : "analyzed", "analyzer": "analyzer_ru", "search_analyzer": 'snowball', "language": "Russian" },
+         "interviewee_bio": { "type": "string", "index" : "not_analyzed"},
+         // title (interviewee name) for exact full-text search (no partial matches)
+         "title": { "type": "string", "index" : "analyzed", "analyzer": "standard" },
+         // The rest are facets which are used for strict match queries or filtering only
+         "published_on": { "type": "string", "index" : "not_analyzed"},
+         "interview_date": { "type": "string", "index" : "not_analyzed"}
+         "interview_location": { "type": "string", "index" : "not_analyzed"}
+
+       }
+      },
+      "fragment": {
+        "_parent": {"type": "interview"},
+        "properties": {
+          "id": { "type": "string", "index" : "not_analyzed" },
+          "type": { "type": "string", "index" : "not_analyzed" },
+          "content": { "type": "string", "index" : "analyzed", "analyzer": "analyzer_ru", "search_analyzer": 'snowball', "language": "Russian" },
+          //"position": { "type": "integer", "index": "not_analyzed" }
+        }
+      }
+    }
+  }
+};
