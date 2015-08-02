2a2926e05c4ad619e205cd177f69f8ad6be2ddd2
Daniel Beilinson <beilinson@gmail.com>
2015-08-02 16:36:08 +0300

    switch to test db

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var config = {
 	// Elastic Search host
   host: 'http://46.101.188.112:9200',
   // Archivist host
-  archive: 'http://ost.d4s.io'
+  archive: 'http://archivist-ost.herokuapp.com'
 };
 
 console.log();
