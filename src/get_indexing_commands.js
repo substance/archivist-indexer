af9c737b54dba4245dd0ecfe51bfdc2c6f414dcf
Oliver Buchtala <oliver.buchtala@gmail.com>
2015-08-04 18:13:24 +0200

    Generate fragment facets.
    For each paragraph there is an array subjects and entities
    referenced within the paragraph.

--- src/get_indexing_commands.js
+++ src/get_indexing_commands.js
@@ -1,4 +1,5 @@
 var cheerio = require('cheerio');
+var _ = require('underscore');
 global.$ = cheerio.load('', {decodeEntities: false});
 
 module.exports = function getIndexingCommands(interview) {
@@ -49,6 +50,23 @@ module.exports = function getIndexingCommands(interview) {
       return;
     }
     var nodeHtml = htmlExporter.convertNode(node).html();
+
+    var facets = [];
+    var path = [node.id, 'content'];
+    var annotations = interview.getIndex('annotations').get(path);
+    _.each(annotations, function(anno) {
+      if (anno.type === "entity_reference") {
+        facets.push(anno.target);
+      }
+    });
+    var annotationFragments = interview.containerAnnotationIndex.getFragments(path, 'content');
+    _.each(annotationFragments, function(annoFragment) {
+      var anno = annoFragment.anno;
+      if (anno.type === "subject_reference") {
+        facets = facets.concat(anno.target);
+      }
+    });
+
     var entryId = nodeId;
     var nodeEntry = { "index" : {
       _index: 'interviews',
@@ -62,7 +80,7 @@ module.exports = function getIndexingCommands(interview) {
         type: type,
         content: nodeHtml,
         position: pos,
-        target: []
+        target: facets
       });
   });
 
