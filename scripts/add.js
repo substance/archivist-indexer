6525af0715da0c021082a2159099f9046803ab4e
Oliver Buchtala <oliver.buchtala@gmail.com>
2015-08-06 14:04:18 +0200

    Better incremental indexing.

--- scripts/add.js
+++ scripts/add.js
@@ -13,36 +13,28 @@ var argv = require("yargs")
   .demand(['i'])
   .argv;
 
-var ArchivistInterview = require('../interview');
-var indexArticle = require('../src/index_interview.js');
 var _ = require('underscore');
-var request = require('superagent');
+var getJSON = require('../src/get_json');
+var Interview = require('archivist-core/interview');
+var indexArticle = require('../src/index_interview.js');
 var elasticsearch = require('elasticsearch');
 var config = require('../config');
-var client = new elasticsearch.Client(_.clone(config));
 
 var interviewUrl = config.archive + '/api/documents/' + argv.i;
 
-var getJSON = function(url, cb) {
-  request
-    .get(url)
-    .end(function(err, res){
-      if(err) console.error(err);
-      cb(err, res.body);
-    });
-}
-
 getJSON(interviewUrl, function(err, json){
-  if (err) return cb(err);
+  if (err) {
+    return console.error(err);
+  }
   console.log('Indexing interview %s...', interviewUrl);
-  var interview = new ArchivistInterview(json);
-
-	indexArticle(client, interview)
-		.error(function() {
-		  console.error("Failed.", arguments);
-		})
-		.done(function() {
-		  console.log("Done.");
-		  client.close();
-		});
+  var interview = new Interview.fromJson(json);
+  var client = new elasticsearch.Client(_.clone(config));
+	indexArticle(client, interview, function(err) {
+    if (err) {
+      console.error("Failed.", err);
+    } else {
+      console.log("Done.");
+    }
+    client.close();
+  });
 });
